0c432f09e1f1e6f5b00e57a1cb4addaf43fe9d2b
Eric Chen <eschen@cpp.edu>
2022-09-06 01:38:16 -0700

    1.8-2.3 Completed

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
